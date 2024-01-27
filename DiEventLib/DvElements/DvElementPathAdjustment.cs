792c0e5c57fff752c260c115ed200028d2dcdb96
Ashrindy <honzula1000@gmail.com>
2024-01-27 11:51:14 +0100

    matrixes should be fixed, even though theyre not 1:1

--- DiEventLib/DvElements/DvElementPathAdjustment.cs
+++ DiEventLib/DvElements/DvElementPathAdjustment.cs
@@ -7,7 +7,7 @@ namespace DiEventLib;
 public class DvElementPathAdjustment : DvNodeObject
 {
     public Vector3 Position;
-    public Quaternion Rotation;
+    public Vector3 Rotation;
     public Vector3 Scale;
     public uint[] Field_40 { get; set; }
 
@@ -17,16 +17,15 @@ public class DvElementPathAdjustment : DvNodeObject
     public override void Read(BinaryObjectReader reader)
     {
         var mtx = reader.Read<Matrix4x4>();
-        Matrix4x4.Decompose(mtx, out Scale, out Rotation, out Position);
+        Quaternion tempRot;
+        Matrix4x4.Decompose(mtx, out Scale, out tempRot, out Position);
+        Rotation = Utils.ToEulerAngles(tempRot);
         Field_40 = reader.ReadArray<uint>(4);
     }
 
     public override void Write(BinaryObjectWriter writer)
     {
-        var mtxPos = Matrix4x4.CreateTranslation(Position);
-        var mtxRot = Matrix4x4.CreateFromQuaternion(Rotation);
-        var mtxSca = Matrix4x4.CreateScale(Scale);
-        writer.Write(mtxPos * mtxRot * mtxSca);
+        writer.Write(Utils.ComposeMatrix(Position, Scale, Utils.ToQuaternion(Rotation)));
         writer.WriteArray(Field_40);
     }
 }
