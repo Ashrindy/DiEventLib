c5e59c82486af1f0cdbb8058eaf1a394a7e6b69d
Ashrindy <honzula1000@gmail.com>
2024-02-11 18:19:22 +0100

    some stuff about the sonic camera element

--- DiEventLib/DvElements/DvElementSonicCamera.cs
+++ DiEventLib/DvElements/DvElementSonicCamera.cs
@@ -4,18 +4,27 @@ namespace DiEventLib;
 
 public class DvElementSonicCamera : DvNodeObject
 {
+    public uint Field_00 { get; set; }
+    public uint[] Field_01 { get; set; }
     public float[] Field_4c { get; set; }
+    public byte[] Data { get; set; }
 
     public DvElementSonicCamera() { }
     public DvElementSonicCamera(BinaryObjectReader reader)
         => Read(reader);
     public override void Read(BinaryObjectReader reader)
     {
-        Field_4c = reader.ReadArray<float>(80);
+        Field_00 = reader.Read<uint>();
+        Field_01 = reader.ReadArray<uint>(3);
+        Field_4c = reader.ReadArray<float>(44);
+        Data = reader.ReadArray<byte>(128);
     }
 
     public override void Write(BinaryObjectWriter writer)
     {
+        writer.Write(Field_00);
+        writer.WriteArray(Field_01);
         writer.WriteArray(Field_4c);
+        writer.WriteArray(Data);
     }
 }
