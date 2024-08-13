31f605ca018db941b7b5589c26f646c9127fd060
Ashrindy <honzula1000@gmail.com>
2024-08-13 10:00:39 +0200

    updated PathInterpolation a bit

--- DiEventLib/DvElements/DvElementPathInterpolation.cs
+++ DiEventLib/DvElements/DvElementPathInterpolation.cs
@@ -4,28 +4,36 @@ using DiEventLib.Misc;
 namespace DiEventLib;
 
 [DvNodeCategory("Element")]
-[DvNodeDescription("Path Interpolation", "Interpolates between path adjustments")]
+[DvNodeDescription("Path Interpolation", "Interpolates path movement")]
 public class DvElementPathInterpolation : DvNodeElement
 {
-    public byte[] Data;
+    public float[] Data;
+    public float[] CurveData;
 
     public DvElementPathInterpolation() : base(DvElementID.PathInterpolation)
     {
-        Data = new byte[592];
-        for (int i = 0; i < 592; i++)
+        Data = new float[20];
+        for (int i = 0; i < 20; i++)
         {
             Data[i] = 0;
+        }
+        CurveData = new float[128];
+        for (int i = 0; i < 128; i++)
+        {
+            CurveData[i] = 0;
         }
     }
     public DvElementPathInterpolation(BinaryObjectReader reader)
         => Read(reader);
     public void Read(BinaryObjectReader reader)
     {
-        Data = reader.ReadArray<byte>(592);
+        Data = reader.ReadArray<float>(20);
+        CurveData = reader.ReadArray<float>(128);
     }
 
     public void Write(BinaryObjectWriter writer)
     {
         writer.WriteArray(Data);
+        writer.WriteArray(CurveData);
     }
 }
